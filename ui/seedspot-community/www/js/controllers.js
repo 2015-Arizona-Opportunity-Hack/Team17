067d2d8bed281660fb90f85d386a3268c7274ac5
Pramodh Natarajan <pramodhn91@gmail.com>
2015-10-11 09:39:34 -0700

    videos from course archive

--- ui/seedspot-community/www/js/controllers.js
+++ ui/seedspot-community/www/js/controllers.js
@@ -23,9 +23,6 @@ angular.module('starter.controllers', [])
 })
 .controller('ContentController', function($scope, Authorization, $ionicModal, $http) {
 
-  //$scope.userdetails = function(){
-  //  $scope.input = Authorization.getAuthObject();
-  //}
 
   $scope.userdetails = Authorization.getAuthObject();
 
@@ -44,21 +41,55 @@ angular.module('starter.controllers', [])
     $scope.modal.remove()
   };
 
-  $scope.clipSrc = 'img/test.MOV';
+  $scope.clipSrc = '';
 
   $scope.courseContent = function() {
     $http({
       method: 'GET',
       url: 'http://127.0.0.1:5000/course_content?username='+$scope.userdetails.username
     }).then(function successCallback(response) {
-        console.log(JSON.stringify(response));
+        var obj1 = response.data.data.files;
+        var i, j;
+        var result = '';
+        var count = 1;
+        for(i = 0; i < obj1.length; i++) {
+          var obj2 = obj1[i];
+          var files = '';
+          var folder = obj2[0].split('/')[2];
+
+          for(j = 0; j < obj2.length; j++) {
+
+            if(obj2[j].split('/')[3].split('.')[1] == 'mp4') {
+              console.log(obj2[j].split('/')[3]);
+              console.log(count);
+              document.getElementById('button'+count).innerHTML = obj2[j].split('/')[3];
+              count = count+1;
+            }
+          }
+        }
+
+        //document.getElementById('clickedContent').innerHTML = (result);
+        //angular.element(document.getElementById('clickedContent')).append(result);
+        document.getElementById('clickedContent').style.visibility = 'visible';
       }, function errorCallback(response) {
         console.log('Oh god!');
       });
   };
 
+  $scope.showContent = function (link) {
+    $http({
+      method: 'GET',
+      url: 'http://127.0.0.1:5000/get_file?filepath='+link
+    }).then(function successCallback(response) {
+      var path = response.data.data.filepath.file_path;
+      $scope.playVideo(path);
+      }, function errorCallback(response) {
+        console.log('Oh god!');
+      });
+  }
+
   $scope.playVideo = function(source) {
-    this.clipSrc = source;
+    $scope.clipSrc = source;
     $scope.showModal('templates/popover.html');
   }
 
